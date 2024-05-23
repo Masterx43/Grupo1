a54198e8f2ab417d6b0bc6923fb4448ba551f27b
Masterx43 <bastiancito.26@gmail.com>
2024-05-23 13:39:39 -0400

    Update saludo.py

--- 007D/saludo.py
+++ 007D/saludo.py
@@ -1,2 +1,6 @@
 print("Hello, World")
 print("Welcome to the python class")
+n1=int(input("ingrese primer numero"))
+n2=int(input("ingrese segundo numero"))
+suma=n1+n2
+print(f"La suma de los valores es {suma})
